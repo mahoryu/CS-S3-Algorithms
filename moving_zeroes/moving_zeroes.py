a6b6d57117f8fc3803af506013fa8e950d4b0df6
Ethan Holden <mahoryu@gmail.com>
2020-08-11 20:24:06 -0600

    Fisrt pass of moving_zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,20 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-
-    pass
+    if not arr.count(0):
+        return arr
+    else:
+        # count of non-zero ints
+        count = 0
+        for i in range(len(arr)):
+            # move all non-zeros to the front
+            if arr[i] != 0:
+                arr[count] = arr[i]
+                count +=1
+        # replace any values after count to be zero
+        for i in range(count,len(arr)):
+            arr[i] = 0
+        return arr
 
 
 if __name__ == '__main__':
