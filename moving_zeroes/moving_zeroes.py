76339888ecbcd7a2df01df3cbf11532684c2f2a4
Ethan Holden <mahoryu@gmail.com>
2020-08-13 19:37:18 -0600

    Assed the runtime of the moving_zeros file

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,6 +2,8 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+import time
+
 def moving_zeroes(arr):
     # Your code here
     if not arr.count(0):
@@ -24,4 +26,10 @@ if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
 
+    start = time.time()
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+    print(f"{time.time() - start:.5f}")
+
+    # the runtime of this function is O(n) because we only have to
+    # run through the array only twice and we drop any leading numbers.
+    # It's linear and the runtime scales with the input data.
